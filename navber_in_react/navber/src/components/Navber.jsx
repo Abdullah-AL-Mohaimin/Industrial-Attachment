2ac263379c97bb85ea6ad9693a1e158fc392ae2b
Al-Mohaimin <mohaiminpc@gmail.com>
2025-11-10 11:36:14 +0600

    Update Responsive Navber

--- navber_in_react/navber/src/components/Navber.jsx
+++ navber_in_react/navber/src/components/Navber.jsx
@@ -11,11 +11,11 @@ const Navber = () => {
     <nav className="bg-black ">
       <div className="h-[75px] text-white items-center flex justify-between">
         {/* menuber */}
-        <button onClick={() => setIsOpen(!isOpen)} className="block sm:hidden px-8 ">
+        <button onClick={() => setIsOpen(!isOpen)} className="block sm:hidden sm:px-8 ">
             <FontAwesomeIcon icon={faBars} size="2x" />
         </button>
         {/* logo */}
-        <div className="px-12">
+        <div className="sm:px-12">
           <a href="">
               <img src={logo} alt="" className="w-[126px] h-[34px]"/>
           </a>
@@ -27,7 +27,7 @@ const Navber = () => {
             <a href=""className="px-4">Discovery</a>
             <a href=""className="px-4">Contuct us</a>
         </div>
-        <div className="px-4">
+        <div className="px-4 flex">
           <a href=""><FontAwesomeIcon icon={faUser} size='2x' /></a>
           <a href=""><FontAwesomeIcon icon={faCartShopping} size='2x' /></a>
         </div>
